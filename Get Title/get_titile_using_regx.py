2642750e144bd3f7c2b93f3f46bfe50d02d6671f
Shilpa Nibin <kvshilpa1993@gmail.com>
2019-05-18 10:02:08 +0530

    Python program to extract title from structured data of  the given url using regx.

---
+++ Get Title/get_titile_using_regx.py
@@ -0,0 +1,20 @@
+import urllib.request
+import re
+
+urls=["https://www.atg.world/#"]
+
+pattern="<title>(.+?)</title>"
+
+result = re.compile( pattern )
+
+
+for url in urls:
+        
+        htmlsource=urllib.request.urlopen(url)
+        
+        htmltext=htmlsource.read().decode('utf-8')
+
+        titles = re.findall(result,htmltext)
+
+        print ( '\n Title identified from the [ ' + str( url ) + ' ] is : \n \n ' + str( titles ))
+        
